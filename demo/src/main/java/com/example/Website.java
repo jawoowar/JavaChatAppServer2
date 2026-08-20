906ef6b117f4914bb95fc8f713c81098107e6c01
Jennifer <99441503+jawoowar@users.noreply.github.com>
2026-08-20 15:23:50 +0100

    made the website not white text on background (probably looks worse but its fine)

--- demo/src/main/java/com/example/Website.java
+++ demo/src/main/java/com/example/Website.java
@@ -20,22 +20,25 @@ public class Website implements HttpHandler {
         String UsrListStr = String.valueOf(usrs.view());
         String UsrCount = String.valueOf(usrs.view().size());
 
+        Runtime runtime = Runtime.getRuntime();
 
-
+        long maxMemory = ((runtime.maxMemory()/ 1024) / 1024);
+        long totalMemory = ((runtime.totalMemory()/ 1024) / 1024);
+        long freeMemory = ((runtime.freeMemory()/ 1024) / 1024);
+        int freeProcesses = runtime.availableProcessors();
 
 
         String pagePath = exchange.getRequestURI().getPath();
         System.out.println(pagePath);
 
-        //builder.append("<html><body>");
-        //builder.append("<h1>" + armount + "</h1>");
-        //builder.append("<h1>" + pagePath + "</h1>");
-        //builder.append("</body></html>");
 
         String file = FileHandler.Load("demo/src/main/resources/index.html");
 
         file = file.replace("{users}", UsrListStr);
         file = file.replace("{userCount}", UsrCount);
+        file = file.replace("{ramUsage}", String.valueOf(totalMemory));
+        file = file.replace("{totalRam}", String.valueOf(freeMemory));
+        file = file.replace("{totalCPU}", String.valueOf(freeProcesses));
 
         if (file != null) {
             byte[] responseBytes = file.getBytes("UTF-8");
